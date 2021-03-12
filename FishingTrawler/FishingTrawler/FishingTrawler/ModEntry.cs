430c9102100110f90a7ee287467679101de269a6
Mr. Automation <31755155+Floogen@users.noreply.github.com>
2021-03-11 18:13:47 -0600

    Start of project

---
+++ FishingTrawler/FishingTrawler/FishingTrawler/ModEntry.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Reflection;
+using FishingTrawler.Patches.Locations;
+using Harmony;
+using StardewModdingAPI;
+
+namespace FishingTrawler
+{
+    public class ModEntry : Mod
+    {
+        internal static IMonitor monitor;
+        internal static IModHelper modHelper;
+
+        // ModData related
+        internal static string offeringsStoredInWaterHutKey;
+
+        public override void Entry(IModHelper helper)
+        {
+            // Set up the monitor, helper and config
+            monitor = Monitor;
+            modHelper = helper;
+        }
+    }
+}
