0bf40337fbd4362ce2c5d09f1dda8871de917694
Mr. Automation <31755155+Floogen@users.noreply.github.com>
2021-04-06 17:56:56 -0500

    Updated comment

--- FishingTrawler/FishingTrawler/FishingTrawler/ModEntry.cs
+++ FishingTrawler/FishingTrawler/FishingTrawler/ModEntry.cs
@@ -305,6 +305,7 @@ namespace FishingTrawler
                         break;
                     case FlagType.SlimeKing:
                         // Consumes all fish but gives a 50% chance of converting each fish into some slime, 25% chance to convert to a Slimejack and a 3% chance to convert into a random slime egg
+                        // Consumes all fish but gives a 50% chance of converting each fish into some slime, 25% chance to convert to a Slimejack and a 1% chance to convert into a random slime egg
                         _trawlerRewards.Value.hasSlimeKing = true;
                         break;
                     default:
