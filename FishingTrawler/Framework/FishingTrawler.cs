46e1ff9100536911864fe3a857ffb280a1259d11
Floogen <31755155+Floogen@users.noreply.github.com>
2023-03-05 21:03:58 -0600

    Implemented debug command ft_skip_requirements

--- FishingTrawler/Framework/FishingTrawler.cs
+++ FishingTrawler/Framework/FishingTrawler.cs
@@ -111,8 +111,9 @@ namespace FishingTrawler
             }
 
             // Add in our debug commands
-            helper.ConsoleCommands.Add("ft_getflags", "Gives all the variations of the ancient flag.\n\nUsage: ft_getflags", DebugGetAllFlags);
-            helper.ConsoleCommands.Add("ft_getspecials", "Gives all the special rewards.\n\nUsage: ft_getspecials", DebugGetSpecialRewards);
+            helper.ConsoleCommands.Add("ft_get_flags", "Gives all the variations of the ancient flag.\n\nUsage: ft_get_flags", DebugGetAllFlags);
+            helper.ConsoleCommands.Add("ft_get_specials", "Gives all the special rewards.\n\nUsage: ft_get_specials", DebugGetSpecialRewards);
+            helper.ConsoleCommands.Add("ft_skip_requirements", "Skips all requirements to meet Murphy and enables the minigame.\n\nUsage: ft_skip_requirements", DebugSkipRequirements);
 
             // Hook into GameLoops related events
             helper.Events.GameLoop.UpdateTicking += OnUpdateTicking;
@@ -982,6 +983,11 @@ namespace FishingTrawler
         {
             Game1.player.addItemByMenuIfNecessary(new AnglerRing());
             Monitor.Log($"Giving all special rewards to {Game1.player}.", LogLevel.Trace);
+        }
+
+        private void DebugSkipRequirements(string command, string[] args)
+        {
+            Game1.player.mailReceived.Add(ModDataKeys.MAIL_FLAG_MURPHY_WAS_INTRODUCED);
         }
     }
 }
