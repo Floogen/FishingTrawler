31882395e4edee700b33484fa407b7bcfba059cd
Floogen <31755155+Floogen@users.noreply.github.com>
2023-03-10 22:34:34 -0600

    Adjusted UI to better fit the screen

--- FishingTrawler/Framework/UI/TrawlerUI.cs
+++ FishingTrawler/Framework/UI/TrawlerUI.cs
@@ -4,6 +4,7 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using StardewValley;
 using StardewValley.BellsAndWhistles;
+using StardewValley.Minigames;
 using System;
 
 namespace FishingTrawler.UI
@@ -11,22 +12,26 @@ namespace FishingTrawler.UI
     internal static class TrawlerUI
     {
         internal static void DrawUI(SpriteBatch b, int fishingTripTimer, int amountOfFish, int floodLevel, bool isHullLeaking, int rippedNetsCount, int fuelLevel)
+        private static float _scale = 2.5f;
+
         {
+            _scale = 2.5f;
             int languageOffset = LocalizedContentManager.CurrentLanguageCode == LocalizedContentManager.LanguageCode.en ? 8 : LocalizedContentManager.CurrentLanguageLatin ? 16 : 8;
 
             b.End();
             b.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend, SamplerState.PointClamp, null, null);
-            b.Draw(FishingTrawler.assetManager.uiTexture, new Vector2(16f, 16f) + new Vector2(-3f, -3f) * 4f, new Rectangle(0, 16, 7, 57), Color.White, 0f, Vector2.Zero, 4f, SpriteEffects.None, 1f - 0.001f);
-            b.Draw(FishingTrawler.assetManager.uiTexture, new Vector2(16f, 16f) + new Vector2(-1f, -3f) * 4f, new Rectangle(2, 16, 74, 57), Color.White, 0f, Vector2.Zero, 4f, SpriteEffects.None, 1f - 0.001f);
-            b.Draw(FishingTrawler.assetManager.uiTexture, new Vector2(16f, 16f) + new Vector2(256 + 4, -12f), new Rectangle(68, 16, 8, 67), Color.White, 0f, Vector2.Zero, 4f, SpriteEffects.None, 1f - 0.001f);
+            b.Draw(FishingTrawler.assetManager.uiTexture, new Vector2(16f, 16f) + new Vector2(-3f, -3f) * _scale, new Rectangle(0, 16, 7, 93), Color.White, 0f, Vector2.Zero, _scale, SpriteEffects.None, 1f - 0.001f);
+            b.Draw(FishingTrawler.assetManager.uiTexture, new Vector2(16f, 16f) + new Vector2(-1f, -3f) * _scale, new Rectangle(2, 16, 74, 93), Color.White, 0f, Vector2.Zero, _scale, SpriteEffects.None, 1f - 0.001f);
+            b.Draw(FishingTrawler.assetManager.uiTexture, new Vector2(16f, 16f) + new Vector2(65f, -3f) * _scale, new Rectangle(68, 16, 8, 103), Color.White, 0f, Vector2.Zero, _scale, SpriteEffects.None, 1f - 0.001f);
 
-            Game1.drawWithBorder($"{FishingTrawler.i18n.Get("ui.flooding.name")}: {floodLevel}%", Color.Black, floodLevel > 75 ? Color.Red : isHullLeaking ? Color.Yellow : Color.White, new Vector2(32f, 24f), 0f, 1f, 1f, tiny: false);
-            Game1.drawWithBorder(string.Concat(FishingTrawler.i18n.Get("ui.nets.name"), ": ", rippedNetsCount < 1 ? FishingTrawler.i18n.Get("ui.generic.working") : rippedNetsCount > 1 ? FishingTrawler.i18n.Get("ui.nets.ripped") : FishingTrawler.i18n.Get("ui.nets.ripping")), Color.Black, rippedNetsCount < 1 ? Color.White : rippedNetsCount > 1 ? Color.Red : Color.Yellow, new Vector2(32f, 76f), 0f, 1f, 1f, tiny: false);
-            Game1.drawWithBorder($"{FishingTrawler.i18n.Get("ui.engine.name")}: {fuelLevel}%", Color.Black, fuelLevel < 30 ? Color.Red : fuelLevel < 50 ? Color.Yellow : Color.White, new Vector2(32f, 128f), 0f, 1f, 1f, tiny: false);
-            b.Draw(FishingTrawler.assetManager.uiTexture, new Vector2(28f, 174f), new Rectangle(0, 0, 16, 16), Color.White, 0f, Vector2.Zero, 3f, SpriteEffects.None, 1f);
-            Game1.drawWithBorder(string.Concat(amountOfFish), Color.Black, Color.White, new Vector2(76f, 169f + languageOffset), 0f, 1f, 1f, tiny: false);
-            b.Draw(FishingTrawler.assetManager.uiTexture, new Vector2(136f, 169f), new Rectangle(16, 0, 16, 16), Color.White, 0f, Vector2.Zero, 4f, SpriteEffects.None, 1f);
-            Game1.drawWithBorder(Utility.getMinutesSecondsStringFromMilliseconds(fishingTripTimer), Color.Black, Color.White, new Vector2(190f, 169f + languageOffset), 0f, 1f, 1f, tiny: false);
+            b.DrawString(Game1.smallFont, $"{FishingTrawler.i18n.Get("ui.flooding.name")}: {floodLevel}%", new Vector2(32f, 24f), floodLevel > 75 ? Color.Red : isHullLeaking ? Color.Yellow : Color.White, 0f, Vector2.Zero, 1f, SpriteEffects.None, 1f);
+            b.DrawString(Game1.smallFont, string.Concat(FishingTrawler.i18n.Get("ui.nets.name"), ": ", rippedNetsCount < 1 ? FishingTrawler.i18n.Get("ui.generic.working") : rippedNetsCount > 1 ? FishingTrawler.i18n.Get("ui.nets.ripped") : FishingTrawler.i18n.Get("ui.nets.ripping")), new Vector2(32f, 60f), rippedNetsCount < 1 ? Color.White : rippedNetsCount > 1 ? Color.Red : Color.Yellow, 0f, Vector2.Zero, 1f, SpriteEffects.None, 1f);
+            b.DrawString(Game1.smallFont, $"{FishingTrawler.i18n.Get("ui.engine.name")}: {fuelLevel}%", new Vector2(32f, 96f), fuelLevel < 30 ? Color.Red : fuelLevel < 50 ? Color.Yellow : Color.White, 0f, Vector2.Zero, 1f, SpriteEffects.None, 1f);
+
+            b.Draw(FishingTrawler.assetManager.uiTexture, new Vector2(28f, 176f), new Rectangle(0, 0, 16, 16), Color.White, 0f, Vector2.Zero, 2f, SpriteEffects.None, 1f);
+            b.DrawString(Game1.smallFont, $"{amountOfFish}", new Vector2(64f, 169f + languageOffset), Color.White, 0f, Vector2.Zero, 1f, SpriteEffects.None, 1f);
+            b.Draw(FishingTrawler.assetManager.uiTexture, new Vector2(96f, 169f), new Rectangle(16, 0, 16, 16), Color.White, 0f, Vector2.Zero, 3f, SpriteEffects.None, 1f);
+            b.DrawString(Game1.smallFont, Utility.getMinutesSecondsStringFromMilliseconds(fishingTripTimer), new Vector2(140f, 169f + languageOffset), Color.White, 0f, Vector2.Zero, 1f, SpriteEffects.None, 1f);
             b.End();
             b.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend, SamplerState.PointClamp, null, null);
         }
